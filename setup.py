9ed1c26534cbc7499aafa4abf819023f63c415af
Hemenson David <hemensond@gmail.com>
2019-08-13 18:07:40 -0300

    criacao primaria do projeto

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vesla-instabot',
+    version='1.0.0',
+    description='Um bot responsável por follow/unfollow',
+    packages=[
+        'vesla_instabot',
+    ],
+    install_requires=[
+    ],
+)
